c1dd1757ca73e80591ad43c1137fd7ed133b91b8
martin garza <martingza13@gmail.com>
2026-06-09 20:48:13 -0600

    Delete frontend/src directory

--- frontend/src/components/TarjetaRifa.jsx
+++
@@ -1,6 +0,0 @@
-import React, { useState } from 'react';
-import { Link } from 'react-router-dom';
-import { motion } from 'framer-motion';
-
-const TarjetaRifa = ({ rifa, index = 0 }) => {
-  const [isHovered, setIsHovered] = useState(false);
